5c6d9025c95b5eb54925b20c4ba0f5d753f4f6ef
Jacob Certain <jace42@gmail.com>
2023-12-22 16:12:45 -0800

    add HttpSpy; for very verbose HttpClient logging

---
+++ HttpSpySample/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using HttpSpy;
+using MelFormatters;
+using Microsoft.Extensions.Logging;
+
+var lf = LoggerFactory
+    .Create(lb => lb.AddHiCaMelFmattr().AddFilter("HttpSpy", LogLevel.Trace));
+var httpClient = new HttpClient(new HttpSpyHandler(lf.CreateLogger<HttpSpyHandler>(), new HttpClientHandler()));
+var rs = await httpClient.GetAsync("http://google.com");
+lf.CreateLogger<Program>()
+    .With("http.rs.status", rs.StatusCode)
+    .With("http.rs.content-length", rs.Content.Headers.ContentLength)
+    .LogInformation("Done.");
