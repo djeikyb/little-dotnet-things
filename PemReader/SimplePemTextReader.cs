bef64342713abe29692bfa06f670f56ec170c2a3
Jacob Certain <jace42@gmail.com>
2024-01-15 17:37:28 -0800

    a nigh uselessly simple pem reader
    Almost, but not quite, useless. Serves at least as an example?

---
+++ PemReader/SimplePemTextReader.cs
@@ -0,0 +1,37 @@
+﻿// https://gist.githubusercontent.com/djeikyb/fb7fe2fe18f334fa82d803aebbc7d740/raw/6c2d2a7c032017fc598c9fefcac8f318ce487e44/read-pem-and-key-with-or-without-tags.cs
+
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace PemReader;
+
+public class SimplePemTextReader
+{
+    public static (X509Certificate2, RSA) Read(string publicCert, string privateKeyPemTaggedOrNot)
+    {
+        var rsa = RSA.Create();
+        if (privateKeyPemTaggedOrNot.StartsWith("-----BEGIN RSA PRIVATE KEY-----"))
+        {
+            rsa.ImportFromPem(privateKeyPemTaggedOrNot);
+        }
+        else
+        {
+            var key = Convert.FromBase64String(privateKeyPemTaggedOrNot);
+            rsa.ImportRSAPrivateKey(key, out _);
+        }
+
+        X509Certificate2 cert;
+        if (publicCert.StartsWith("-----BEGIN CERTIFICATE-----"))
+        {
+            cert = X509Certificate2.CreateFromPem(publicCert, rsa.ExportRSAPrivateKeyPem());
+        }
+        else
+        {
+            var bytes = Convert.FromBase64String(publicCert);
+            cert = new X509Certificate2(bytes);
+            cert = cert.CopyWithPrivateKey(rsa);
+        }
+
+        return (cert, rsa);
+    }
+}
